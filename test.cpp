75ab8b48ae58982ecdb0fb170b7efb2f2864192c
CadenC04 <126933250+CadenC04@users.noreply.github.com>
2023-04-17 21:59:16 +0000

    Better Looking Code

--- test.cpp
+++ test.cpp
@@ -3,60 +3,97 @@
 #include <math.h>
 using namespace std;
 
-void solveSides(double& a, double& b, double& c, double A, double B, double C) {
-	double AA = (A * (M_PI/180));
-	double BB = (B * (M_PI/180));
-	double CC = (C * (M_PI/180));
-	if (a != 0) {
-		b = (a*sin(BB)/sin(AA));
-		c = (a*sin(CC)/sin(AA));
-	} else if (b != 0) {
-		a = (b*sin(AA)/sin(BB));
-		c = (b*sin(CC)/sin(BB));
-	} else {
-		a = (c*sin(AA)/sin(CC));
-		b = (c*sin(BB)/sin(CC));
+void solveSides(double &a, double &b, double &c, double A, double B, double C)
+{
+	double AA = (A * (M_PI / 180));
+	double BB = (B * (M_PI / 180));
+	double CC = (C * (M_PI / 180));
+	if (a != 0)
+	{
+		b = (a * sin(BB) / sin(AA));
+		c = (a * sin(CC) / sin(AA));
 	}
-	if (a < 0 || b < 0 || c < 0 || A < 0 || B < 0 || C < 0) {
-		cout << "Invalid Triangle" << "\n" << "\n";
-		return;
-	} else if ((A + B + C) > 180) {
-		cout << "Invalid Triangle" << "\n" << "\n";
-		return;
-	} else if ((A + B + C) < 180) {
-		cout << "Invalid Triangle" << "\n" << "\n";
+	else if (b != 0)
+	{
+		a = (b * sin(AA) / sin(BB));
+		c = (b * sin(CC) / sin(BB));
+	}
+	else
+	{
+		a = (c * sin(AA) / sin(CC));
+		b = (c * sin(BB) / sin(CC));
+	}
+	if (a < 0 || b < 0 || c < 0 || A < 0 || B < 0 || C < 0)
+	{
+		cout << "Invalid Triangle"
+			 << "\n"
+			 << "\n";
 		return;
 	}
-	cout << "\n" << "Angle A, B, C: " << A << ", " << B << ", " << C;
-	cout << "\n" << "Side a, b, c: " << a << ", " << b << ", " << c << "\n" << "\n" << "\n";
+	else if ((A + B + C) > 180)
+	{
+		cout << "Invalid Triangle"
+			 << "\n"
+			 << "\n";
+		return;
+	}
+	else if ((A + B + C) < 180)
+	{
+		cout << "Invalid Triangle"
+			 << "\n"
+			 << "\n";
+		return;
+	}
+	cout << "\n"
+		 << "Angle A, B, C: " << A << ", " << B << ", " << C;
+	cout << "\n"
+		 << "Side a, b, c: " << a << ", " << b << ", " << c << "\n"
+		 << "\n"
+		 << "\n";
 }
 
-void getAngles(double a, double b, double c, double& A, double& B, double& C) {
-	if (A == 0) {
+void getAngles(double a, double b, double c, double &A, double &B, double &C)
+{
+	if (A == 0)
+	{
 		A = (180 - B - C);
-	} else if (B == 0) {
+	}
+	else if (B == 0)
+	{
 		B = (180 - A - C);
-	} else {
+	}
+	else
+	{
 		C = (180 - A - B);
 	}
 	solveSides(a, b, c, A, B, C);
 }
 
-void getAngEquation(double a, double b, double c, double A, double B, double C) {
+void getAngEquation(double a, double b, double c, double A, double B, double C)
+{
 	string type;
-	if (A != 0) {
+	if (A != 0)
+	{
 		type += "1";
-	} else {
+	}
+	else
+	{
 		type += "0";
 	}
-	if (B != 0) {
+	if (B != 0)
+	{
 		type += "1";
-	} else {
+	}
+	else
+	{
 		type += "0";
 	}
-	if (C != 0) {
+	if (C != 0)
+	{
 		type += "1";
-	} else {
+	}
+	else
+	{
 		type += "0";
 	}
 	int Type = stoi(type);
@@ -84,50 +121,82 @@ void getAngEquation(double a, double b, double c, double A, double B, double C) 
 	}
 }
 
-void solveAngles(double a, double b, double c, double& A, double& B, double& C) {
-	A = (acos(((b * b) + (c * c) - (a * a)) / (2 * b * c))) * (180/M_PI);
-	B = (acos(((c * c) + (a * a) - (b * b)) / (2 * c * a))) * (180/M_PI);
-	C = (acos(((a * a) + (b * b) - (c * c)) / (2 * a * b))) * (180/M_PI);
-	if (a < 0 || b < 0 || c < 0 || A < 0 || B < 0 || C < 0) {
-		cout << "Invalid Triangle" << "\n" << "\n";
-		return;
-	} else if ((A + B + C) > 180) {
-		cout << "Invalid Triangle" << "\n" << "\n";
-		return;
-	} else if ((A + B + C) < 180) {
-		cout << "Invalid Triangle" << "\n" << "\n";
+void solveAngles(double a, double b, double c, double &A, double &B, double &C)
+{
+	A = (acos(((b * b) + (c * c) - (a * a)) / (2 * b * c))) * (180 / M_PI);
+	B = (acos(((c * c) + (a * a) - (b * b)) / (2 * c * a))) * (180 / M_PI);
+	C = (acos(((a * a) + (b * b) - (c * c)) / (2 * a * b))) * (180 / M_PI);
+	if (a < 0 || b < 0 || c < 0 || A < 0 || B < 0 || C < 0)
+	{
+		cout << "Invalid Triangle"
+			 << "\n"
+			 << "\n";
 		return;
 	}
-	cout << "\n" << "Angle A, B, C: " << A << ", " << B << ", " << C;
-	cout << "\n" << "Side a, b, c: " << a << ", " << b << ", " << c << "\n" << "\n" << "\n";
+	else if ((A + B + C) > 180)
+	{
+		cout << "Invalid Triangle"
+			 << "\n"
+			 << "\n";
+		return;
+	}
+	else if ((A + B + C) < 180)
+	{
+		cout << "Invalid Triangle"
+			 << "\n"
+			 << "\n";
+		return;
+	}
+	cout << "\n"
+		 << "Angle A, B, C: " << A << ", " << B << ", " << C;
+	cout << "\n"
+		 << "Side a, b, c: " << a << ", " << b << ", " << c << "\n"
+		 << "\n"
+		 << "\n";
 }
 
-void pythagThero(double& a, double& b, double& c, double& A, double& B, double& C) {
-	if (a == 0) {
+void pythagThero(double &a, double &b, double &c, double &A, double &B, double &C)
+{
+	if (a == 0)
+	{
 		a = sqrt((c * c) - (b * b));
-	} else if (b == 0) {
+	}
+	else if (b == 0)
+	{
 		b = sqrt((c * c) - (a * a));
-	} else {
+	}
+	else
+	{
 		c = sqrt((a * a) + (b * b));
 	}
 	solveAngles(a, b, c, A, B, C);
 }
 
-void getEquation(double a, double b, double c, double A, double B, double C) {
+void getEquation(double a, double b, double c, double A, double B, double C)
+{
 	string type;
-	if (a != 0) {
+	if (a != 0)
+	{
 		type += "1";
-	} else {
+	}
+	else
+	{
 		type += "0";
 	}
-	if (b != 0) {
+	if (b != 0)
+	{
 		type += "1";
-	} else {
+	}
+	else
+	{
 		type += "0";
 	}
-	if (c != 0) {
+	if (c != 0)
+	{
 		type += "1";
-	} else {
+	}
+	else
+	{
 		type += "0";
 	}
 	int Type = stoi(type);
@@ -137,7 +206,7 @@ void getEquation(double a, double b, double c, double A, double B, double C) {
 	case 0:
 		cout << "Invalid Parameters";
 		break;
-	case 111: 
+	case 111:
 		solveAngles(a, b, c, A, B, C);
 		break;
 	case 110:
@@ -155,45 +224,72 @@ void getEquation(double a, double b, double c, double A, double B, double C) {
 	}
 }
 
-void getData(double& a, double& b, double& c, double& A, double& B, double& C) {
-	cout << "          /|C" << "\n" << "         / |" << "\n";
-	cout << "        /  |" << "\n" << "       /   |" << "\n";
-	cout << "     b/    |a" << "\n" << "     /     |" << "\n";
-	cout << "    /      |" << "\n" << "  A/_______|B" << "\n" << "       c"; 
-	cout << "\n" << "\n";
+void getData(double &a, double &b, double &c, double &A, double &B, double &C)
+{
+	cout << "          /|C"
+		 << "\n"
+		 << "         / |"
+		 << "\n";
+	cout << "        /  |"
+		 << "\n"
+		 << "       /   |"
+		 << "\n";
+	cout << "     b/    |a"
+		 << "\n"
+		 << "     /     |"
+		 << "\n";
+	cout << "    /      |"
+		 << "\n"
+		 << "  A/_______|B"
+		 << "\n"
+		 << "       c";
+	cout << "\n"
+		 << "\n";
 	cout << "Enter side a: ";
-	cina: cin >> a;
-	if (a < 0) {
+cina:
+	cin >> a;
+	if (a < 0)
+	{
 		cout << "Please enter a non-negative number: ";
 		goto cina;
 	}
 	cout << "Enter side b: ";
-	cinb: cin >> b;
-	if (b < 0) {
+cinb:
+	cin >> b;
+	if (b < 0)
+	{
 		cout << "Please enter a non-negative number: ";
 		goto cinb;
 	}
 	cout << "Enter side c: ";
-	cinc: cin >> c;
-	if (c < 0) {
+cinc:
+	cin >> c;
+	if (c < 0)
+	{
 		cout << "Please enter a non-negative number: ";
 		goto cinc;
 	}
 	cout << "Enter angle A: ";
-	cinA: cin >> A;
-	if (A < 0) {
+cinA:
+	cin >> A;
+	if (A < 0)
+	{
 		cout << "Please enter a non-negative number: ";
 		goto cinA;
 	}
 	cout << "Enter angle B: ";
-	cinB: cin >> B;
-	if (B < 0) {
+cinB:
+	cin >> B;
+	if (B < 0)
+	{
 		cout << "Please enter a non-negative number: ";
 		goto cinB;
 	}
 	cout << "Enter angle C: ";
-	cinC: cin >> C;
-	if (C < 0) {
+cinC:
+	cin >> C;
+	if (C < 0)
+	{
 		cout << "Please enter a non-negative number: ";
 		goto cinC;
 	}
@@ -201,12 +297,16 @@ void getData(double& a, double& b, double& c, double& A, double& B, double& C) {
 	getEquation(a, b, c, A, B, C);
 }
 
-int main() {
+int main()
+{
 	double a, b, c, A, B, C;
 	cout << "This program will calculate the remaining ";
-	cout << "angles and/or sides for the triangle." << "\n";
+	cout << "angles and/or sides for the triangle."
+		 << "\n";
 	cout << "If the data you are being asked to input is ";
-	cout << "unknown then just enter 0." << "\n" << "\n";
+	cout << "unknown then just enter 0."
+		 << "\n"
+		 << "\n";
 	getData(a, b, c, A, B, C);
 	return 0;
 }
