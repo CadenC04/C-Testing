0292c1bd4a04c075be28d82f00df3079cffbc6c6
CadenC04 <126933250+CadenC04@users.noreply.github.com>
2023-04-17 19:23:21 +0000

    uhhh

--- test.cpp
+++ test.cpp
@@ -3,93 +3,87 @@
 using namespace std;
 
 /*
-	cout << "Enter angle A: ";
-	cin >> A;
-	cout << "Enter angle B: ";
-	cin >> B;
-	cout << "Enter angle C: ";
-	cin >> C;
+000 // null
+111 // all sides solve angles
+110 // 2 sides pythag then solve angles
+011 // 2 sides pythag then solve angles
+101 // 2 sides pythag then solve angles
+
 */
 
-double solveAngles(double a, double b, double c, double A, double B, double C) {
-	if (A == -1) {
-		//solve for A
-		return acos(((b * b) + (c * c) - (a * a)) / (2 * b * c));
-	} else if (B == -1) {
-		//solve for B
-		return acos(((c * c) + (a * a) - (b * b)) / (2 * c * a));
-	} else if (C == -1) {
-		//solve for C
+void getEquation(double a, double b, double c, double A, double B, double C) {
+	string type;
+	if (a != 0) {
+		type += "1";
+	} else {
+		type += "0";
+	}
+	if (b != 0) {
+		type += "1";
+	} else {
+		type += "0";
+	}
+	if (c != 0) {
+		type += "1";
+	} else {
+		type += "0";
+	}
+	int Type = stoi(type);
+
+	switch (Type)
+	{
+	case 0:
+		cout << "Null";
+		break;
+	case 111: 
+		cout << "111";
+		break;
+	case 110:
+		cout << "110";
+		break;
+	case 11:
+		cout << "011";
+		break;
+	case 101:
+		cout << "101";
+		break;
+	default:
+		break;
 	}
 }
 
-double pythagThe(double x, double y, double z) {
-	if (z != 0) {
-		if (x != 0) {
-			return sqrt((z * z) - (x * x));
-		}
-		else {
-			return sqrt((z * z) - (y * y));
-		}
-	}
-	else {
-            return sqrt((x * x) + (y * y));
-	}
-}
 
-int getMissing() {
-	double a;
-	double b;
-	double c;
-	double A;
-	double B;
-	double C;
-	cout << "          /|C" << "\n" << "         / |" << "\n";
-	cout << "        /  |" << "\n" << "       /   |" << "\n";
-	cout << "     c/    |b" << "\n" << "     /     |" << "\n";
-	cout << "    /      |" << "\n" << "  A/_______|B" << "\n" << "       a"; 
-	cout << "\n" << "\n";
+
+
+
+
+
+
+
+void getData() {
+	double a, b, c, A, B, C;
 	cout << "Enter side a: ";
 	cin >> a;
 	cout << "Enter side b: ";
 	cin >> b;
 	cout << "Enter side c: ";
 	cin >> c;
-
-	cout << "Side A: " << a << " Side B: " << b << " Side C: " << c << " " << "\n" << "Output: ";
-
-	if (a != 0 && b != 0 || a != 0 && c != 0 || b != 0 && c != 0) {
-		if (a == 0) {
-			a = pythagThe(a, b, c);
-		}
-		else if (b == 0) {
-			b = pythagThe(a, b, c);
-		}
-		else {
-			c = pythagThe(a, b, c);
-		}
-    } else if (a != 0 && b != 0 && c != 0) {
-                // can solve for angles
-				A = solveAngles(a, b, c, -1, 0, 0);
-				B = solveAngles(a, b, c, 0, -1, 0);
-				C = solveAngles(a, b, c, 0, 0, -1);
-
-	} else if (a == 0 && b == 0 && c == 0) {
-				cout << "Enter angle A: ";
-				cin >> A;
-				cout << "Enter angle B: ";
-				cin >> B;
-				cout << "Enter angle C: ";
-				cin >> C;
-                // cannot get side lengths
-    } else {
-
-	}
-
-	cout << "Side A: " << a << " Side B: " << b << " Side C: " << c << " " << "\n";
-
-	return 0;
+	cout << "Enter angle A: ";
+	cin >> A;
+	cout << "Enter angle B: ";
+	cin >> B;
+	cout << "Enter angle C: ";
+	cin >> C;
+	getEquation(a, b, c, A, B, C);
 }
+
+/*
+	cout << "          /|C" << "\n" << "         / |" << "\n";
+	cout << "        /  |" << "\n" << "       /   |" << "\n";
+	cout << "     c/    |b" << "\n" << "     /     |" << "\n";
+	cout << "    /      |" << "\n" << "  A/_______|B" << "\n" << "       a"; 
+	cout << "\n" << "\n";
+*/
 
 
 int main() {
@@ -97,6 +91,27 @@ int main() {
 	cout << "angles and/or sides for the triangle." << "\n";
 	cout << "If the data you are being asked to input is ";
 	cout << "unknown then just enter 0." << "\n" << "\n";
-	getMissing();
+	getData();
 	return 0;
 }
+
+
+
+
+/*
+	if (A != 0) {
+		type += "1";
+	} else {
+		type += "0";
+	}
+	if (B != 0) {
+		type += "1";
+	} else {
+		type += "0";
+	}
+	if (C != 0) {
+		type += "1";
+	} else {
+		type += "0";
+	}
+*/
