2671b9315652c5698f948875f0669f998babf2bf
CadenC04 <126933250+CadenC04@users.noreply.github.com>
2023-04-21 17:03:56 +0000

    ahhhh

--- prog.cpp
+++ prog.cpp
@@ -5,21 +5,27 @@
 
 using namespace std;
 
-void calcCent(int points[][2], int pCount) {
+void calcCentX(int points[][2], int pCount) {
 double Cx;
-double Cy;
 double Aix;
-double Aiy;
     for (int i = 0; i < pCount; i++) {
         Aix += points[i][0];
-        Aiy += points[i][1];
         cout << points[i][0] << " ";
-        cout << points[i][1] << " ";
     }
     Cx = (Aix / pCount);
-    Cy = (Aiy / pCount);
-    cout << Aix << " " << Aiy << "\n";
+    cout << Aix << "\n";
     cout << "Centroid X: " << Cx << "\n";
+}
+
+void calcCentY(int points[][2], int pCount) {
+double Cy;
+double Aiy;
+    for (int i = 0; i < pCount; i++) {
+        Aiy += points[i][1];
+        cout << points[i][1] << " ";
+    }
+    Cy = (Aiy / pCount);
+    cout << Aiy << "\n";
     cout << "Centroid Y: " << Cy << "\n";
 }
 
@@ -40,11 +46,15 @@ void getData() {
         points[i][0] = x;
         points[i][1] = y;
     }
-    calcCent(points, pCount);
+    calcCentX(points, pCount);
+    calcCentY(points, pCount);
 }
 
 void breakdownShape() {
-    
+    int sCount;
+    cout << "Please breakdown the complex shape into simple shapes" << "\n";
+    cout << "Enter the amount of simple shapes: ";
+    cin >> sCount;
 }
 
 
