937ead72a0c1e1bc13465e848e0674b81936a5dd
CadenC04 <126933250+CadenC04@users.noreply.github.com>
2023-04-23 04:25:01 +0000

    more work

--- prog.cpp
+++ prog.cpp
@@ -5,70 +5,199 @@
 
 using namespace std;
 
-void calcCentX(int points[][2], int pCount) {
-double Cx;
-double Aix;
-    for (int i = 0; i < pCount; i++) {
-        Aix += points[i][0];
-        cout << points[i][0] << " ";
-    }
-    Cx = (Aix / pCount);
-    cout << Aix << "\n";
-    cout << "Centroid X: " << Cx << "\n";
-}
-
-void calcCentY(int points[][2], int pCount) {
-double Cy;
-double Aiy;
-    for (int i = 0; i < pCount; i++) {
-        Aiy += points[i][1];
-        cout << points[i][1] << " ";
-    }
-    Cy = (Aiy / pCount);
-    cout << Aiy << "\n";
-    cout << "Centroid Y: " << Cy << "\n";
-}
-
-
-
-void getData() {
-    int pCount;
-    cout << "Enter # of points: ";
-    cin >> pCount;
-    int points[pCount][2];
-    for (int i = 0; i < pCount; i++) {
-        int x;
-        int y;
-        cout << "Enter point x" << i + 1 << ": ";
-        cin >> x; 
-        cout << "Enter point y" << i + 1 << ": ";
-        cin >> y;
-        points[i][0] = x;
-        points[i][1] = y;
-    }
-    calcCentX(points, pCount);
-    calcCentY(points, pCount);
-}
-
-void breakdownShape() {
-    int sCount;
-    cout << "Please breakdown the complex shape into simple shapes" << "\n";
-    cout << "Enter the amount of simple shapes: ";
-    cin >> sCount;
-}
-
-
+void breakdownShape();
+void getData(int val, double& x, double& y, double& a);
+void getRect(double& x, double& y, double& a);
+void getTriangle(double & x, double& y, double& a);
+void getCircle(int subType, double & x, double& y, double& a);
+void getCircularSector();
+void getArc();
+void getSpandrel();
+void signum(double& a);
 
 int main() {
     string type;
-    cout << "Please enter the type of shape: (complex or simple N-Sided polygon)";
+    cout << "Is it a 'complex', or a 'simple' shape?: ";
     cin >> type;
     if (type == "complex") {
         breakdownShape();
     } else {
-        getData();
+        return 0;
     }
     return 0;
+}
+
+void breakdownShape() {
+    int sCount;
+    double Cx;
+    double Cy;
+    double Ca;
+    cout << "How many simple shapes make up the composite shape?: ";
+    cin >> sCount;
+    double shapes[sCount][3];
+    for (int i = 0; i < sCount; i++) {
+        double x;
+        double y;
+        double a;
+        int type;
+        cout << "Please base your coordinates off of 0, 0." << "\n";
+        cout << "Please enter the number corresponding to the shape you wish to enter first: " << "\n";
+        cout << "1) Rectangle/Square" << "\n" << "2) Triangle" << "\n" << "3) Circular Shape" << "\n" << "4) Arc" << "\n" "5) Spandrel" "\n";
+        cout << "Enter Here: ";
+        cin >> type;
+        getData(type, x, y, a);
+        shapes[i][0] = x;
+        shapes[i][1] = y;
+        shapes[i][2] = a;
+    }
+}
+
+void getData(int type, double& x, double& y, double& a) {
+    switch (type)
+    {
+    case 1:
+        getRect(x, y, a);
+        break;
+    case 2:
+        getTriangle(x, y, a);
+        break;
+    case 3:
+        int subType;
+        cout << "1) Circle" << "\n" << "2) Semi-Circle" << "\n" << "3) Quarter-Circle" << "\n" << "4) Circular Sector" << "\n" << "Enter here: ";
+        switch (subType)
+        {
+        case 1:
+            getCircle(1, x, y, a);
+            break;
+        case 2:
+            getCircle(2, x, y, a); //semi
+            break;
+        case 3:
+            getCircle(3, x, y, a);//quarter
+            break;
+        case 4:
+            getCircularSector();
+            break;
+        default:
+            break;
+        }
+        break;
+    case 4:
+        int subType;
+        cout << "1) Segment Arc" << "\n" << "2) Semicircular Arc" << "\n" << "Enter here: ";
+        cin >> subType;
+        switch (subType)
+        {
+        case 1:
+            getArc(); //segment arc
+            break;
+        case 2:
+            getArc(); //semicircular arc
+            break;
+        default:
+            break;
+        }
+        break;
+    case 5:
+        getSpandrel(); // what is this thing?
+        break;
+    default:
+        break;
+    }
+}
+
+void getRect(double& x, double& y, double& a) {
+    double points[4][2];
+    for (int i = 0; i < 4; i++) {
+        double xx;
+        double yy;
+        cout << "Please enter X" << i << ": ";
+        cin >> xx;
+        cout << "Please enter Y" << i << ": ";
+        cin >> yy;
+        points[i][0] = xx;
+        points[i][1] = yy;
+    }
+    x = ((points[0][0] + points[1][0] + points[2][0] + points[3][0]) / 4);
+    y = ((points[0][1] + points[1][1] + points[2][1] + points[3][1]) / 4);
+    a = (sqrt(pow((points[1][0] - points [0][0]), 2) + pow((points[1][1] - points[0][1]), 2)) * sqrt(pow((points[3][0] - points [0][0]), 2) + pow((points[3][1] - points[0][1]), 2)));
+    signum(a);
+}
+
+void getTriangle(double & x, double& y, double& a) {
+    double points[3][2];
+    for (int i = 0; i < 3; i++) {
+        double xx;
+        double yy;
+        cout << "Please enter X" << i << ": ";
+        cin >> xx;
+        cout << "Please enter Y" << i << ": ";
+        cin >> yy;
+        points[i][0] = xx;
+        points[i][1] = yy;
+    }
+    x = ((points[0][0] + points[1][0] + points[2][0]) / 3);
+    y = ((points[0][1] + points[1][1] + points[2][1]) / 3);
+    a = (((points[1][1] - points[0][1]) * (points[2][0] - points[0][0])) / 2);
+    signum(a);
+}
+
+void getCircle(int subType, double & x, double& y, double& a) {
+    double xx;
+    double yy;
+    double rr;
+    cout << "Please enter X: ";
+    cin >> xx;
+    cout << "Please enter Y: ";
+    cin >> yy;
+    cout << "Please enter r: ";
+    cin >> rr;
+    if (subType == 1) {
+        x = xx;
+        y = yy;
+        a = (M_PI * pow(rr, 2));
+    } else if (subType == 2) {
+        double angle;
+        cout << "Please enter the degrees of rotation: ";
+        cin >> angle;
+        double theta = (angle * (M_PI/180));
+        double ty;
+        ty = ((4 * rr) / (3 * M_PI));
+        x = xx;
+        y = (ty * (sin(theta)));
+        a = ((M_PI * pow(rr, 2)) / 2);
+    } else {
+        double angle;
+        cout << "Please enter the degrees of rotation: ";
+        cin >> angle;
+        double theta = (angle * (M_PI/180));
+        double ty;
+        ty = ((4 * rr) / (3 * M_PI));
+        x = (ty * (cos(theta)));
+        y = (ty * (sin(theta)));
+        a = ((M_PI * pow(rr, 2)) / 4);
+    }
+}
+/*
+void getCircularSector();
+void getArc();
+void getSpandrel();
+*/
+
+
+
+
+
+
+void signum(double& a) {
+    int space;
+    cout << "1) Positive Space" << "\n" << "2) Negative Space" << "\n" << "Enter here: ";
+    cin >> space;
+    if (space == 0) {
+        a = (a * (-1));
+    } else {
+        a = a;
+    }
 }
 
 //https://owlcation.com/stem/How-to-Solve-Centroids-of-Compound-Shapes
